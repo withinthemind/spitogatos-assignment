5937b13e0e4660a5fb99f419e12f6045ec11be65
Alexandros Ioannidis <alex@withinthemind.gr>
2021-02-24 10:14:47 +0200

    responsive and slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,50 @@
 $(document).ready(function(){
 
+	// burger menu
+	$('nav .icon').on('click', function () {
+		$(this).toggleClass('expanded');
+		if ($('nav .icon').hasClass('expanded')) {
+			$('.menu').addClass('responsive');
+			$('body').addClass('overflow');
+		} 
+		else {
+			$('.menu').removeClass('responsive');
+			$('body').removeClass('overflow');
+		}
+	});	
+
+	// slider
+	var slideIndex = 1;
+	showSlides(slideIndex);
+
+	$('.slider-dot').on('click', function () {
+		var index = $(this).data('slide');
+		showSlides(slideIndex = index);
+	});
+
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("slider-img");
+	  var dots = document.getElementsByClassName("slider-dot");
+	  if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+		  slides[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+		  dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";
+	  dots[slideIndex-1].className += " active";
+	}		
+
 	// function to show input error
 	function showError(element) {
 		
 		$(element).addClass('error');
 		$(element).next('label').addClass('error');
 		$(element).nextAll('span.checkmark').css('display', 'none');
+		$(element).nextAll('span.help-text').html('Error message').addClass('error');
 		
 	}
 	
@@ -14,37 +53,39 @@ $(document).ready(function(){
 		
 		$(element).removeClass('error');
 		$(element).next('label').removeClass('error');
-		$(element).nextAll('span.checkmark').css('display', 'inline-block');	
+		$(element).nextAll('span.checkmark').css('display', 'inline-block');
+		$(element).nextAll('span.help-text').html('Help Text').removeClass('error');		
 	
 	}
-
 	// floating label input
-    $('.floated input').focusout(function(){
-		
-        var text_value = $(this).val();
+	$(function () {
 
-        if (text_value === "") {
-            $(this).removeClass('float-label');
-        }
-		else {
-           $(this).addClass('float-label');
-        }
-		
-    });
+		$('.input-box .label').on('click', function () {
+			$(this).closest('.input-box').find('input').focus();
+		});
+		$('.input-box input').on('keyup', function () {
+			var value = $.trim($(this).val());
+			if (value) {
+				$(this).closest('.input-box').addClass('hasValue');
+			} else {
+				$(this).closest('.input-box').removeClass('hasValue');
+			}
+		});
 
-	// floating label textarea	
-    $('.floated textarea').focusout(function(){
-		
-        var text_value = $(this).val();
+		$('.input-box .label').on('click', function () {
+			$(this).closest('.input-box').find('textarea').focus();
+		});
 
-        if (text_value === "") {
-            $(this).removeClass('float-label');
-        }
-		else {
-           $(this).addClass('float-label');
-        }
-		
-    });	
+		$('.input-box textarea').on('keyup', function () {
+			var value = $.trim($(this).val());
+			if (value) {
+				$(this).closest('.input-box').addClass('hasValue');
+			} else {
+				$(this).closest('.input-box').removeClass('hasValue');
+			}
+		});
+
+	});	
 	
 	// textarea validation
 	$('#message').on('input', function(){
@@ -101,6 +142,12 @@ $(document).ready(function(){
 		hideError('#phone');
 		}	
 	});
+
+	// remove errors when user is typing
+    $(document).on('keyup', 'input.error', function () {
+        $(this).removeClass('error');
+		$(this).next('label').removeClass('error');
+    });
 	
 	// form submit
 	$('#submit').click(function() {
@@ -119,6 +166,9 @@ $(document).ready(function(){
 		subs = data;
 		$.each(data,function(key, value) {
 			$('#category').append('<option value=' + value['categoryId'] + '>' + value['name'] + '</option>');
+				$.each(value['subCategories'],function(key, value) {
+					$('#subcategory').append('<option value=' + value['subCategoryId'] + '>' + value['name'] + '</option>');
+				});			
 		});		
 	});
 	
@@ -127,10 +177,17 @@ $(document).ready(function(){
 		var category = $(this).val();
 		$('#subcategory').html('');
 			$.each(subs,function(key, value) {
-				if (category == value['categoryId']) {
+				if (category == 0) {
 					$.each(value['subCategories'],function(key, value) {
 						$('#subcategory').append('<option value=' + value['subCategoryId'] + '>' + value['name'] + '</option>');
-					});
+					});					
+				}
+				else {
+					if (category == value['categoryId']) {
+						$.each(value['subCategories'],function(key, value) {
+							$('#subcategory').append('<option value=' + value['subCategoryId'] + '>' + value['name'] + '</option>');
+						});
+					}
 				}
 			});		
 	});	
